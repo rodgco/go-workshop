436b907b3da797f9bc3c5d8587e80814368a8679
Rodrigo Couto <r@rodg.co>
2024-08-09 18:44:42 -0300

    01 fmt-y dumptied

--- basics/01fmt.go
+++ basics/01fmt.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("hello young gopher")
@@ -8,12 +11,15 @@ func main() {
 	a := "double"
 	b := "rainbow"
 	// TODO print `a` and `b` to the console as part of a sentence
+	fmt.Printf("a is %v and b %v.\n", a, b)
 
 	const c = 42
 	const d = 6.022e23
 	// TODO use math.Min to find the minimum of the two constants `c` and `d` and print the result to the console
+	fmt.Printf("Minimum is %v.\n", math.Min(c, d))
 
 	// TODO add a variable `e` to make the following console message return true
 	const powerLevel = 9000
+	e := 10_000
 	fmt.Printf("It's over 9000? %v\n", e > powerLevel)
 }
